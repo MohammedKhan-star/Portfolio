49ac505ce0da127493778206275ec199e16ed24d
Mohammed Khan <your-github-email@gmail.com>
2026-02-10 14:00:23 +0530

    project update

--- data/projects.ts
+++ data/projects.ts
@@ -4,34 +4,34 @@ export const projects = [
     description:
       "Full-stack MERN application with AI chatbot, user authentication, and online payments using Stripe.",
     link: "https://food-delivery-app-zomoto.vercel.app/",
-    image: "/project/food.PNG"
+    image: "/project/food.png"
   },
   {
     title: "Learning Management System (LMS)",
     description:
       "MERN stack LMS with role-based dashboards, courses, assignments, and admin panel.",
     link: "https://lms-sandy-six.vercel.app/",
-    image: "/project/LMS6.PNG"
+    image: "/project/lms6.png"
   },
   {
     title: "AI E-Commerce Platform",
     description:
       "Next.js e-commerce platform with AI-powered product recommendations and admin panel.",
     link: "https://gocart-xqlp.vercel.app/",
-    image: "/project/AIEcommerce2.PNG"
+    image: "/project/alecommerce2.png"
   },
   {
     title: "Video Streaming Platform (React.js, Vite, YouTube API)",
     description:
       "Next.js e-commerce platform with AI-powered product recommendations and admin panel.",
     link: "https://youtube-clone-4-glwf.onrender.com/",
-    image: "/project/Youtubeclone1.PNG"
+    image: "/project/youtubeclone1.png"
   },
   {
     title: "E-COMMERCE PLATFORM (NXTTRENDZ)",
     description:
       "Next.js e-commerce platform with AI-powered product recommendations and admin panel.",
     link: "https://khannxttreandz.ccbp.tech/login",
-    image: "/project/ecommerce.PNG"
+    image: "/project/ecommerce.png"
   }
 ];
